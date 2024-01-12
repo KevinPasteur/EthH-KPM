be60e87517c4eb05759f8bc6332b912abef23fd6
KevinPasteur <kevin.pasteur@icloud.com>
2024-01-12 20:51:09 +0100

    Remove /ping control

--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,3 @@ use Illuminate\Support\Facades\DB;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/ping', function (Request  $request) {
-    $connection = DB::connection('mongodb');
-    $msg = 'MongoDB is accessible!';
-    try {
-        $connection->command(['ping' => 1]);
-    } catch (\Exception  $e) {
-        $msg = 'MongoDB is not accessible. Error: ' . $e->getMessage();
-    }
-    return ['msg' => $msg];
-});
